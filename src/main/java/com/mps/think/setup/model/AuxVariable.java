875c2312b50e6ad56f0307eacdf05d648faed151
Akshay pathak <rohit.a.negi@gmail.com>
2022-11-08 10:15:56 +0530

    AUX

---
+++ src/main/java/com/mps/think/setup/model/AuxVariable.java
@@ -0,0 +1,108 @@
+package com.mps.think.setup.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Table(name = "aux_variable")
+@Entity
+public class AuxVariable extends BaseEntity {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@Column(name = "aux_var_id")
+	private Integer auxVarId;
+
+	@OneToOne
+	@JoinColumn(name = "pub_id", referencedColumnName = "id")
+	private Publisher pubId;
+
+	@Column(name = "aux_category")
+	private String auxCategory;// need to changes in enum  Customer ,CustomerAddress ,Oder
+
+	@Column(name = "variable_name")
+	private String variableName;
+
+	@Column(name = "variable_type")
+	private String variableType;// int string enum
+
+	@Column(name = "defult_value")
+	private String defultValue;
+
+	@Column(name = "status")
+	private Integer auxStatus;
+
+	public Integer getAuxVarId() {
+		return auxVarId;
+	}
+
+	public void setAuxVarId(Integer auxVarId) {
+		this.auxVarId = auxVarId;
+	}
+
+	public String getAuxCategory() {
+		return auxCategory;
+	}
+
+	public void setAuxCategory(String auxCategory) {
+		this.auxCategory = auxCategory;
+	}
+
+	public String getVariableName() {
+		return variableName;
+	}
+
+	public void setVariableName(String variableName) {
+		this.variableName = variableName;
+	}
+
+	public String getVariableType() {
+		return variableType;
+	}
+
+	public void setVariableType(String variableType) {
+		this.variableType = variableType;
+	}
+
+	public String getDefultValue() {
+		return defultValue;
+	}
+
+	public void setDefultValue(String defultValue) {
+		this.defultValue = defultValue;
+	}
+
+	public Integer getAuxStatus() {
+		return auxStatus;
+	}
+
+	public void setAuxStatus(Integer auxStatus) {
+		this.auxStatus = auxStatus;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	public Publisher getPubId() {
+		return pubId;
+	}
+
+	public void setPubId(Publisher pubId) {
+		this.pubId = pubId;
+	}
+
+	@Override
+	public String toString() {
+		return "AuxVariable [auxVarId=" + auxVarId + ", pubId=" + pubId + ", auxCategory=" + auxCategory
+				+ ", variableName=" + variableName + ", variableType=" + variableType + ", defultValue=" + defultValue
+				+ ", auxStatus=" + auxStatus + "]";
+	}
+	
+}
