7e2fdc460216b1f19d6bd883493a828f8add8464
AkshayPathakAhom <114207199+AkshayPathakAhom@users.noreply.github.com>
2022-11-15 09:57:51 +0530

    Update PublisherController.java

--- src/main/java/com/mps/think/setup/controller/PublisherController.java
+++ src/main/java/com/mps/think/setup/controller/PublisherController.java
@@ -5,9 +5,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.mps.think.setup.service.PublisherSerivce;
 
+@RestController
 public class PublisherController {
 	
 	@Autowired 
