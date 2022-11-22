50618434872c7712d629ef4e48c173805d7fd7b3
Akshay-R <114207199+AkshayPathakAhom@users.noreply.github.com>
2022-11-22 09:41:45 -0800

    SourceAttribute

---
+++ src/main/java/com/mps/think/setup/controller/SourceAttributeController.java
@@ -0,0 +1,46 @@
+package com.mps.think.setup.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mps.think.setup.serviceImpl.SourceAttributeServiceImpl;
+import com.mps.think.setup.vo.SourceAttributeVo;
+import com.mps.think.setup.vo.VolumeGroupVO;
+
+@RestController
+@CrossOrigin
+public class SourceAttributeController {
+	@Autowired
+	SourceAttributeServiceImpl sourceAttributeServiceImpl;
+
+	@RequestMapping(value = "/findAllSourceAttribute", method = RequestMethod.GET)
+	public ResponseEntity<?> findAllSourceAttribute() {
+
+		return ResponseEntity.ok(sourceAttributeServiceImpl.findAllSourceAttribute());
+	}
+
+	@RequestMapping(value = "/saveSourceAttribute", method = RequestMethod.POST)
+	public ResponseEntity<?> saveSourceAttribute(@Valid @RequestBody SourceAttributeVo sourceAttributeVo) {
+
+		return ResponseEntity.ok(sourceAttributeServiceImpl.saveSourceAttribute(sourceAttributeVo));
+	}
+
+	@PostMapping("/updateSourceAttribute")
+	public ResponseEntity<?> updateSourceAttribute(@RequestBody SourceAttributeVo sourceAttributeVo) {
+		return ResponseEntity.ok(sourceAttributeServiceImpl.updateSourceAttribute(sourceAttributeVo));
+	}
+
+@RequestMapping(value = "/findbySourceAttributeId", method = RequestMethod.POST)
+public ResponseEntity<?> findbySourceAttributeId(@Valid @RequestBody Integer sourceAttributeId) {
+
+	return ResponseEntity.ok(sourceAttributeServiceImpl.findbySourceAttributeId(sourceAttributeId));
+}
+}
--- src/main/java/com/mps/think/setup/serviceImpl/SourceAttributeServiceImpl.java
+++ src/main/java/com/mps/think/setup/serviceImpl/SourceAttributeServiceImpl.java
@@ -2,36 +2,50 @@ package com.mps.think.setup.serviceImpl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.mps.think.setup.model.SourceAttribute;
+import com.mps.think.setup.repo.SourceAttributeRepo;
 import com.mps.think.setup.service.SourceAttributeService;
 import com.mps.think.setup.vo.SourceAttributeVo;
+
 @Service
 public class SourceAttributeServiceImpl implements SourceAttributeService {
 
+	@Autowired
+	SourceAttributeRepo sourceAttributeRepo;
+
 	@Override
 	public List<SourceAttribute> findAllSourceAttribute() {
-		// TODO Auto-generated method stub
-		return null;
+		return sourceAttributeRepo.findAll();
 	}
 
 	@Override
-	public SourceAttributeVo saveSourceAttribute(SourceAttributeVo surceAttributeVo) {
-		// TODO Auto-generated method stub
-		return null;
+	public SourceAttributeVo saveSourceAttribute(SourceAttributeVo sourceAttributeVo) {
+		SourceAttribute sourceAttribute = new SourceAttribute();
+		sourceAttribute.setSourceAttribute(sourceAttributeVo.getSourceAttribute());
+		sourceAttribute.setDescription(sourceAttributeVo.getDescription());
+		sourceAttribute.setWidth(sourceAttributeVo.getWidth());
+		sourceAttribute = sourceAttributeRepo.save(sourceAttribute);
+		sourceAttributeVo.setId(sourceAttribute.getId());
+		return sourceAttributeVo;
 	}
 
 	@Override
 	public SourceAttributeVo updateSourceAttribute(SourceAttributeVo sourceAttributeVo) {
-		// TODO Auto-generated method stub
-		return null;
+		SourceAttribute sourceAttribute = new SourceAttribute();
+		sourceAttribute.setId(sourceAttributeVo.getId());
+		sourceAttribute.setSourceAttribute(sourceAttributeVo.getSourceAttribute());
+		sourceAttribute.setDescription(sourceAttributeVo.getDescription());
+		sourceAttribute.setWidth(sourceAttributeVo.getWidth());
+		sourceAttribute = sourceAttributeRepo.save(sourceAttribute);
+		return sourceAttributeVo;
 	}
 
 	@Override
 	public SourceAttribute findbySourceAttributeId(Integer sourceAttributeId) {
-		// TODO Auto-generated method stub
-		return null;
+		return sourceAttributeRepo.findById(sourceAttributeId).get();
 	}
 
 }
