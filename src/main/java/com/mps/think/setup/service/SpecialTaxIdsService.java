dead4493c934f8024c0c45007aa3c4f49cf22ed0
Bimlesh Kumar <Bimlesh.Kumar@macmill.com>
2022-12-05 12:21:02 +0530

    THW-149 commiting

---
+++ src/main/java/com/mps/think/setup/service/SpecialTaxIdsService.java
@@ -0,0 +1,22 @@
+package com.mps.think.setup.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.mps.think.setup.model.SpecialTaxIds;
+import com.mps.think.setup.vo.SpecialTaxIdsVO;
+
+@Service
+public interface SpecialTaxIdsService {
+	
+	public List<SpecialTaxIds> findAllSpecialTaxIds();
+
+	public SpecialTaxIdsVO saveSpecialTaxIds(SpecialTaxIdsVO specialTaxIds);
+
+	public SpecialTaxIdsVO updateSpecialTaxIds(SpecialTaxIdsVO specialTaxIds);
+
+	public SpecialTaxIds findbySpecialTaxIdsId(Integer SpecialTaxIdsId);
+
+
+}
