8a288aadf437993a40dade70b4fafd283f542fc7
Akshay-R <114207199+AkshayPathakAhom@users.noreply.github.com>
2022-12-05 10:17:17 -0800

    Key Line Controller

---
+++ src/main/java/com/mps/think/setup/controller/KeyLineController.java
@@ -0,0 +1,41 @@
+package com.mps.think.setup.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mps.think.setup.serviceImpl.LableKeyLineServiceImpl;
+import com.mps.think.setup.vo.LableKeyLineVO;
+
+@RestController
+@CrossOrigin
+public class KeyLineController {
+
+	@Autowired
+	LableKeyLineServiceImpl lableKeyLineServiceImpl;
+
+	@GetMapping("/findAllLableKeyLine")
+	public ResponseEntity<?> findAllLableKeyLine() {
+		return ResponseEntity.ok(lableKeyLineServiceImpl.findAllLableKeyLine());
+	}
+
+	@PostMapping("/saveLableKeyLine")
+	public ResponseEntity<?> saveLableKeyLine(@RequestBody LableKeyLineVO lableKeyLineVO) {
+		return ResponseEntity.ok(lableKeyLineServiceImpl.saveLableKeyLine(lableKeyLineVO));
+	}
+
+	@PostMapping("/updateLableKeyLine")
+	public ResponseEntity<?> updateLableKeyLine(@RequestBody LableKeyLineVO lableKeyLineVO) {
+		return ResponseEntity.ok(lableKeyLineServiceImpl.updateLableKeyLine(lableKeyLineVO));
+	}
+
+	@PostMapping("/findbyLableKeyLineById")
+	public ResponseEntity<?> findbyLableKeyLineById(@RequestBody Integer lableKeyLineId) {
+		return ResponseEntity.ok(lableKeyLineServiceImpl.findbyLableKeyLine(lableKeyLineId));
+	}
+
+}
