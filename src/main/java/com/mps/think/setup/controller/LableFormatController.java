dc0954fa75f250608b5eef3046e7420e466444ac
Samir Anand <Samir.Anand@macmill.com>
2022-12-20 16:18:27 +0530

    THW-124new

--- src/main/java/com/mps/think/setup/controller/LableFormatController.java
+++ src/main/java/com/mps/think/setup/controller/LableFormatController.java
@@ -3,6 +3,7 @@ package com.mps.think.setup.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -36,6 +37,11 @@ public class LableFormatController {
 	@PostMapping("/findbyLableFormatId")
 	public ResponseEntity<?> findbyLableFormatId(@RequestBody Integer lableFormatId) {
 		return ResponseEntity.ok(lableFormatServiceImp.findbyLableFormat(lableFormatId));
+	}
+	
+	@DeleteMapping("/deleteByLableFormatId")
+	public ResponseEntity<?> deleteByLableFormatId(@RequestBody Integer lableFormatId) {
+		return ResponseEntity.ok(lableFormatServiceImp.deleteByLableFormatId(lableFormatId));
 	}
 
 }
